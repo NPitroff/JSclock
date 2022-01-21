c2f9006dff0c927f3b18964c529a604c6892e033
NPitroff <nicolai.pitroff@gmail.com>
2022-01-20 19:58:13 -0800

    Added an update to append numbers less than 10

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 function currentTime() {
-  var date = new Date();
+  var date = new Date(); //Allows you to pull hours, minutes, and seconds
   var hour = date.getHours();
   var min = date.getMinutes();
-  var sec = date.getSecond();
-}
+  var sec = date.getSeconds();
 
-console.log(currentTime());
+  // nest the variables for updateTime in currentTime();
+
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+
+  console.log(currentTime());
+}
+// append the time returned with a "0"
+function updateTime(k){
+  if(k<10){
+    return "0"+ k;
+  }
+  else {
+    return k;
+  }
+}
