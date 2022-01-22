3dd06df1ea2f82bb7a2f38405fdbce7ed1b32dfb
NPitroff <nicolai.pitroff@gmail.com>
2022-01-22 08:42:30 -0800

    Added JS framework for 12-clock

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// 24-Hour Clock JS
 function currentTime() {
   var date = new Date(); //Allows you to pull hours, minutes, and seconds
   var hour = date.getHours();
@@ -25,3 +26,32 @@ function updateTime(k){
 }
 
 currentTime();
+
+// 12-Hour CLock JS
+function current12Time() {
+  var date = new Date(); //Allows you to pull hours, minutes, and seconds
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+
+  // nest the variables for updateTime in currentTime();
+
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  // display the time in the html
+  document.getElementById("12-clock").innerHTML = hour + " : " + min + " : " + sec;
+setTimeout(current12Time, 1000);
+
+}
+// append the time returned with a "0"
+function updateTime(k){
+  if(k<10){
+    return "0"+ k;
+  }
+  else {
+    return k;
+  }
+}
+
+current12Time();
